ff29e08d4b5c4c2c392cc5fe4d618730af6cca92
Jiang Ning <neo@desktop.home>
2024-07-15 16:58:25 +0800

    add window min width and height

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    minWidth: 300,
+    minHeight: 160,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
